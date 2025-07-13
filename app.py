ef1bfe8d8e4483f8556f6b5a390512b5ca56de44
Ronisugandi <roniisugandi@gmail.com>
2025-07-13 07:20:29 +0700

    update semua halaman

--- app.py
+++ app.py
@@ -63,5 +63,12 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
